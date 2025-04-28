b94851f80ac047a5cfecf5dcc6931cb98feae3e1
Muhammad Kashif Pathan <mkpathan5426@gmail.com>
2025-04-29 01:58:54 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1285,3 +1285,75 @@ document.addEventListener('DOMContentLoaded', () => {
     // Initial check for auto-delete
     checkAutoDelete();
 });
+
+
+
+
+
+
+
+let string = "";
+let inputField = document.querySelector('.input');
+
+document.querySelectorAll('.button').forEach((button) => {
+    button.addEventListener('click', (e) => {
+        const buttonText = e.target.innerHTML;
+
+        switch (buttonText) {
+            case '=':
+                try {
+                    string = eval(string);
+                    inputField.value = string;
+                } catch (error) {
+                    inputField.value = "Error";
+                }
+                break;
+
+            case 'C':
+                string = "";
+                inputField.value = string;
+                break;
+
+            case 'Del':
+                string = string.slice(0, -1);
+                inputField.value = string;
+                break;
+
+            case '%':
+                try {
+                    // If the last character is an operator, remove it before calculating percentage
+                    const lastChar = string.charAt(string.length - 1);
+                    if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') {
+                        string = string.slice(0, -1);
+                    }
+
+                    string = (parseFloat(eval(string)) / 100).toString();
+                    inputField.value = string;
+                } catch (error) {
+                    inputField.value = "Error";
+                }
+                break;
+
+            default:
+                string += buttonText;
+                inputField.value = string;
+                break;
+        }
+    });
+});
+
+
+const calulatorContainer = document.querySelector('.calculator-container');
+const openCalculatorBtn = document.querySelector('.open-calculator-btn');
+const closeCalculatorBtn = document.querySelector('.close-calculator-btn');
+
+openCalculatorBtn.addEventListener('click', () => {
+    calulatorContainer.classList.remove('show');
+    console.log("open")
+}
+);
+closeCalculatorBtn.addEventListener('click', () => {
+    calulatorContainer.classList.add('show');
+    console.log("close")
+}
+);
